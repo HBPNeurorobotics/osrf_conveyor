204f4b3702ac4f4f5999930d4c8dfc83822cf618
dhood <dhood@users.noreply.github.com>
2017-02-23 15:01:43 -0800

    Drop part onto tray

--- test_ariac/test_gripper.py
+++ test_ariac/test_gripper.py
@@ -10,9 +10,6 @@ from ariac_example import ariac_example
 import rospy
 import rostest
 
-from trajectory_msgs.msg import JointTrajectory
-from trajectory_msgs.msg import JointTrajectoryPoint
-
 
 class Tester(unittest.TestCase):
 
@@ -20,7 +17,11 @@ class Tester(unittest.TestCase):
         self.comp_class = ariac_example.MyCompetitionClass()
         ariac_example.connect_callbacks(self.comp_class)
         time.sleep(1.0)
+
+        # Pre-defined pose that puts the gripper in contact with a part.
         self._send_arm_to_part()
+
+        # Enable the gripper so that it picks up the part.
         success = ariac_example.control_gripper(True)
         self.assertTrue(success, 'Gripper not successfully controlled')
         time.sleep(1.0)
@@ -29,10 +30,39 @@ class Tester(unittest.TestCase):
         self.assertTrue(
             self.comp_class.current_gripper_state.attached, 'Part not successfully attached')
 
+        # Move the part over the tray using a pre-defined sequence of poses.
+        self._send_arm_to_tray()
+        self.assertTrue(
+            self.comp_class.current_gripper_state.enabled, 'Gripper no longer enabled')
+        self.assertTrue(
+            self.comp_class.current_gripper_state.attached, 'Part no longer attached')
+
+        # Disable the gripper so that it drops the part.
+        success = ariac_example.control_gripper(False)
+        self.assertTrue(success, 'Gripper not successfully controlled')
+        time.sleep(1.0)
+        self.assertFalse(
+            self.comp_class.current_gripper_state.enabled, 'Gripper not successfully disabled')
+        self.assertFalse(
+            self.comp_class.current_gripper_state.attached, 'Part not successfully dettached')
+
+        time.sleep(1.0)
+
     def _send_arm_to_part(self):
         positions = [1.85, 0.35, -0.38, 2.76, 3.67, -1.51, 0.0]
         self.comp_class.send_arm_to_state(positions)
         time.sleep(1.5)
+
+    def _send_arm_to_tray(self):
+        trajectory = [
+            [1.76, 0.28, -1.38, 2.76, 3.27, -1.51, 0.0],
+            [1.76, 0.38, -1.38, 1.5, 3.27, -1.51, 0.0],
+            [1.76, 2.06, -1.38, 1.5, 3.27, -1.51, 0.0],
+            [1.76, 2.06, -0.63, 1.5, 3.27, -1.51, 0.0],
+        ]
+        for positions in trajectory:
+            self.comp_class.send_arm_to_state(positions)
+            time.sleep(1.0)
 
 
 if __name__ == '__main__':
@@ -44,7 +74,7 @@ if __name__ == '__main__':
         print('Waiting for Gazebo to start...')
         time.sleep(1.0)
     # Take an extra nap, to allow plugins to be loaded
-    time.sleep(10.0)
+    time.sleep(12.0)
     print('OK, starting test.')
 
     rostest.run('test_ariac', 'test_gripper', Tester, sys.argv)
